16620a11b4b95076d551bdb22fa21d2be0f958fd
yangin <422918408@qq.com>
2021-09-25 11:48:50 +0800

    CHORE: 添加speed-measure-webpack-plugin插件

--- scripts/webpack.prod.config.js
+++ scripts/webpack.prod.config.js
@@ -1,7 +1,10 @@
 const { merge } = require('webpack-merge')
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
 const webpackConfigBase = require('./webpack.base.config')
+
+const smp = new SpeedMeasurePlugin()
 
 const webpackConfigProd = {
   mode: 'production'
 }
-module.exports = merge(webpackConfigBase, webpackConfigProd)
+module.exports = smp.wrap(merge(webpackConfigBase, webpackConfigProd))
